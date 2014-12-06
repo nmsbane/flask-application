79c94aa81eb2c25d43252045ff43d5fc11c80559
madhusudan <madhusudhan638@gmail.com>
2014-12-06 13:36:59 +0530

    error templates

--- hello.py
+++ hello.py
@@ -1,9 +1,21 @@
 from flask import Flask, render_template
 from flask.ext.script import Manager
+from flask.ext.bootstrap import Bootstrap
 
 app = Flask(__name__)
 
 manager = Manager(app)
+bootstrap = Bootstrap(app)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
 
 
 @app.route('/')
