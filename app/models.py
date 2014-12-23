1b1592f5f74bdf6eb699299162553001ffeb5e75
madhusudan <madhusudhan638@gmail.com>
2014-12-23 22:51:48 +0530

    user profile forms are done

--- app/models.py
+++ app/models.py
@@ -3,6 +3,7 @@ from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 from flask import current_app
 from flask.ext.login import UserMixin, AnonymousUserMixin
 from . import db, login_manager
+from datetime import datetime
 
 
 class Permission:
@@ -54,6 +55,12 @@ class User(UserMixin, db.Model):
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
     password_hash = db.Column(db.String(128))
     confirmed = db.Column(db.Boolean, default=False)
+    name = db.Column(db.String(64))
+    location = db.Column(db.String(64))
+    about_me = db.Column(db.Text())
+    member_since = db.Column(db.DateTime(), default=datetime.utcnow)
+    last_seen = db.Column(db.DateTime(), default=datetime.utcnow)
+
 
     def __init__(self, **kwargs):
         super(User, self).__init__(**kwargs)
@@ -126,6 +133,11 @@ class User(UserMixin, db.Model):
         self.email = new_email
         db.session.add(self)
         return True
+
+    def ping(self):
+        self.last_seen = datetime.utcnow()
+        db.session.add(self)
+
 
     def can(self, permissions):
         return self.role is not None and \
