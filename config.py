2fab53a67e340f003038b279131142b79694fc9c
madhusudan <madhusudhan638@gmail.com>
2015-01-07 22:55:59 +0530

    remove usernames and passwords for a gmail account

--- config.py
+++ config.py
@@ -10,10 +10,14 @@ class Config:
     MAIL_PORT = 587
     MAIL_USE_TLS = True
     FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     FLASKY_MAIL_SENDER = 'Flasky Admin <flasky@example.com>'
+    FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
     FLASKY_POSTS_PER_PAGE = 20
     FLASKY_FOLLOWERS_PER_PAGE = 50
     FLASKY_COMMENTS_PER_PAGE = 30
+
 
     @staticmethod
     def init_app(app):
